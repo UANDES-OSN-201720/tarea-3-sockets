10a69c66a52da8f52db07ac42d651fba84d67e65
ditarud <ditarud@miuandes.cl>
2017-11-24 12:07:20 -0300

    Cambios

--- Client.py
+++ Client.py
@@ -1,63 +1,22 @@
-import sys
 import socket
-import select
-
-
-def chat_client():
-    if (len(sys.argv) < 3):
-        print 'Usage : python chat_client.py hostname port'
-        sys.exit()
-
-    host = sys.argv[1]
-    # host = socket.gethostname()
-    port = int(sys.argv[2])
-
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    s.settimeout(2)
-
-    # connect to remote host
-    try:
-        # s.connect(('localhost', 9009))
-        s.connect((host, port))
-        print "host is " + host
-        print "port is " + str(port)
-    except:
-        print 'Unable to connect on ' + host + " " + str(port)
-        sys.exit()
-
-    print 'Connected to remote host. You can start sending messages'
-    sys.stdout.write('<= ');
-    sys.stdout.flush()
-
+from thread import *
+from time import sleep
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('127.0.0.1', 8889))
+u = raw_input('Username: ')
+print 'To send: recipient>message'
+def se(s):
     while 1:
-
-        socket_list = [sys.stdin, s]
-
-        # Get the list sockets which are readable
-        ready_to_read, ready_to_write, in_error = select.select(socket_list, [], [])
-
-        for sock in ready_to_read:
-            if sock == s:
-                # incoming message from remote server, s
-                data = sock.recv(4096)
-                if not data:
-                    print '\nDisconnected from chat server'
-                    sys.exit()
-                else:
-                    # print data
-                    sys.stdout.write(data)
-                    sys.stdout.write('<= ');
-                    sys.stdout.flush()
-
-            else:
-                # user entered a message
-                msg = sys.stdin.readline()
-                s.send(msg)
-                sys.stdout.write('<= ');
-                sys.stdout.flush()
-
-
-if __name__ == "__main__":
-
-    sys.exit(chat_client())
+        s.send(u + '>' + raw_input())
+        print s.recv(1024)
+def re(s):
+    while 1:
+        s.send(u + '>show>')
+        r = s.recv(1024)
+        if r != 'No messages':
+            print r
+        sleep(0.05)
+start_new_thread(se ,(s,))
+start_new_thread(re ,(s,))
+while 1:
+    pass
